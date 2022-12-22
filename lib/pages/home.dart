8bc44473a0828fd70ce2197813c005f979e6bd0e
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-22 13:24:31 +0200

    positioned icons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,18 +14,26 @@ class Home extends StatelessWidget {
         actions: [
           Row(
             children: [
-              Container(
-                child: Text(
-                  "8",
-                  style: TextStyle(
-                      fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
-                ),
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(211, 164, 255, 193),
-                    shape: BoxShape.circle),
+              Stack(
+                children: [
+                  Positioned(
+                    bottom: 24,
+                    child: Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle),
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
               ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
               Padding(
                 padding: const EdgeInsets.only(right: 12),
                 child: Text(
