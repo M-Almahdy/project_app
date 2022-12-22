2630d71040939e36a8f5e27805b019e3e7dd64aa
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-22 22:51:58 +0200

    appear and colored the items in home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,7 +10,11 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body:GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/2, crossAxisSpacing: 10, mainAxisSpacing: 33), itemCount: 4, )
+          body:GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/2, crossAxisSpacing: 10, mainAxisSpacing: 33), itemCount: 4, itemBuilder: (BuildContext context, int (context, index) {
+            return Container(
+              color: Colors.amber,
+            );
+          }),),
 
 
 
