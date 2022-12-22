5e1490b872af91c10f51fbac734087f9eb81798b
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 00:20:53 +0200

    create a class for items

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,16 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:test_app/shared/color.dart';
+
+class Item {
+  String imgPath;
+  Float price;
+
+  Item({required this.imgPath, required this.price});
+}
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
