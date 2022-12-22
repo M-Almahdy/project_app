2b269d8eaf3996fc3ff58e53cce2629ee612943d
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-22 22:55:46 +0200

    fix the error

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,11 +10,18 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body:GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/2, crossAxisSpacing: 10, mainAxisSpacing: 33), itemCount: 4, itemBuilder: (BuildContext context, int (context, index) {
+          body: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3/2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 33),
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
             return Container(
               color: Colors.amber,
             );
-          }),),
+          },),
 
 
 
