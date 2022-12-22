7596f5913780f438f846059c1ed689c5bae0cb1c
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 00:47:17 +0200

    delete the list of items from home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,16 +7,7 @@ import 'package:test_app/shared/color.dart';
 
 
 
-List items = [
-  Item(imgPath: "assets/img/1.jpeg", price: 119.99),
-  Item(imgPath: "assets/img/2.jpeg", price: 230.99),
-  Item(imgPath: "assets/img/3.jpeg", price: 199.99),
-  Item(imgPath: "assets/img/4.jpeg", price: 129.99),
-  Item(imgPath: "assets/img/5.jpeg", price: 299.99),
-  Item(imgPath: "assets/img/6.jpeg", price: 154.99),
-  Item(imgPath: "assets/img/7.jpeg", price: 599.99),
-  Item(imgPath: "assets/img/8.jpeg", price: 699.99),
-];
+
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
