1a61e05701b880c17a2973a3add01da7a3345f7b
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 09:43:16 +0200

    to show whole the text with scrolling

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -49,100 +49,103 @@ class Details extends StatelessWidget {
           backgroundColor: appbarGreen,
           title: Text("Details_screen"),
         ),
-        body: Column(
-          children: [
-            Image.asset("assets/img/1.jpeg"),
-            SizedBox(height: 12),
-            Text(
-              "\$ 12.99",
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(4),
-                  child: Text(
-                    "New",
-                    style: TextStyle(fontSize: 20),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/img/1.jpeg"),
+              SizedBox(height: 12),
+              Text(
+                "\$ 12.99",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    child: Text(
+                      "New",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 129, 129),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 255, 129, 129),
-                    borderRadius: BorderRadius.circular(4),
+                  SizedBox(
+                    width: 13,
                   ),
-                ),
-                SizedBox(
-                  width: 13,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                    Icon(
-                      Icons.star,
-                      size: 26,
-                      color: Color.fromARGB(255, 255, 191, 0),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: 55,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.edit_location,
-                      size: 27,
-                      color: Color.fromARGB(168, 3, 65, 27),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Text(
-                      "Flower Shop",
-                      style: TextStyle(fontSize: 19),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "Details : ",
-                    style: TextStyle(fontSize: 22),
-                    textAlign: TextAlign.start,
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text("Most computer keyboards look the same — three rows of letters, a row or two of numbers, and a lot of punctuation and function keys. But although they look similar, keyboards can vary wildly when it comes to how they work on the inside. Most people today use one of two kinds of keyboards: membrane and mechanical keyboards. Membrane keyboards, the more common type, feature some sort of rubber dome under each key. When you press a key, the rubber is compressed, and the key makes an electrical connection with the keyboard's wires. Mechanical keyboards are very different — instead of rubber, they have a plastic switch under the key that makes the connection go through. This might not sound that different, but the style of key has a huge impact on how you type.")
-              ],
-            )
-          ],
+                  SizedBox(
+                    width: 55,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.edit_location,
+                        size: 27,
+                        color: Color.fromARGB(168, 3, 65, 27),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "Flower Shop",
+                        style: TextStyle(fontSize: 19),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      "Details : ",
+                      style: TextStyle(fontSize: 22),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                      "Most computer keyboards look the same — three rows of letters, a row or two of numbers, and a lot of punctuation and function keys. But although they look similar, keyboards can vary wildly when it comes to how they work on the inside. Most people today use one of two kinds of keyboards: membrane and mechanical keyboards. Membrane keyboards, the more common type, feature some sort of rubber dome under each key. When you press a key, the rubber is compressed, and the key makes an electrical connection with the keyboard's wires. Mechanical keyboards are very different — instead of rubber, they have a plastic switch under the key that makes the connection go through. This might not sound that different, but the style of key has a huge impact on how you type.")
+                ],
+              )
+            ],
+          ),
         ));
   }
 }
