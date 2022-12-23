7a4121991ff9ef68afe62851378546e01e97f9ce
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 22:23:30 +0200

    add name for each product in the main list

--- lib/model/items.dart
+++ lib/model/items.dart
@@ -9,12 +9,12 @@ class Item {
 }
 
 List items = [
-  Item(imgPath: "assets/img/1.jpeg", price: 119.99),
-  Item(imgPath: "assets/img/2.jpeg", price: 230.99, location: "ali express"),
-  Item(imgPath: "assets/img/3.jpeg", price: 199.99),
-  Item(imgPath: "assets/img/4.jpeg", price: 129.99),
-  Item(imgPath: "assets/img/5.jpeg", price: 299.99, location: "ali express"),
-  Item(imgPath: "assets/img/6.jpeg", price: 154.99),
-  Item(imgPath: "assets/img/7.jpeg", price: 599.99, location: "Amazon"),
-  Item(imgPath: "assets/img/8.jpeg", price: 699.99, location: "Amazon"),
+  Item(name:"Product 1", imgPath: "assets/img/1.jpeg", price: 119.99),
+  Item(name:"Product 2", imgPath: "assets/img/2.jpeg", price: 230.99, location: "ali express"),
+  Item(name:"Product 3", imgPath: "assets/img/3.jpeg", price: 199.99),
+  Item(name:"Product 4", imgPath: "assets/img/4.jpeg", price: 129.99),
+  Item(name:"Product 5", imgPath: "assets/img/5.jpeg", price: 299.99, location: "ali express"),
+  Item(name:"Product 6", imgPath: "assets/img/6.jpeg", price: 154.99),
+  Item(name:"Product 7", imgPath: "assets/img/7.jpeg", price: 599.99, location: "Amazon"),
+  Item(name:"Product 8", imgPath: "assets/img/8.jpeg", price: 699.99, location: "Amazon"),
 ];
