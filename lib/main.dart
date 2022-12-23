198027f7a64c470c518069370519517f6fd05390
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 16:27:30 +0200

    import package provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:test_app/pages/details_screen.dart';
 import 'package:test_app/pages/home.dart';
 import 'package:test_app/pages/login.dart';
 import 'package:test_app/pages/register.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: Home());
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Home());
   }
 }
