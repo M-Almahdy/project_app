64cb5b17eac769639b1dd2df1b19db0ceead834d
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 14:55:03 +0200

    add location as string and define it in each item

--- lib/model/items.dart
+++ lib/model/items.dart
@@ -1,17 +1,20 @@
 class Item {
   String imgPath;
   double price;
-
-  Item({required this.imgPath, required this.price});
+  String location;
+  Item(
+      {required this.imgPath,
+      required this.price,
+      this.location = "any Electronic Store"});
 }
 
 List items = [
   Item(imgPath: "assets/img/1.jpeg", price: 119.99),
-  Item(imgPath: "assets/img/2.jpeg", price: 230.99),
+  Item(imgPath: "assets/img/2.jpeg", price: 230.99, location: "ali express"),
   Item(imgPath: "assets/img/3.jpeg", price: 199.99),
   Item(imgPath: "assets/img/4.jpeg", price: 129.99),
-  Item(imgPath: "assets/img/5.jpeg", price: 299.99),
+  Item(imgPath: "assets/img/5.jpeg", price: 299.99, location: "ali express"),
   Item(imgPath: "assets/img/6.jpeg", price: 154.99),
-  Item(imgPath: "assets/img/7.jpeg", price: 599.99),
-  Item(imgPath: "assets/img/8.jpeg", price: 699.99),
+  Item(imgPath: "assets/img/7.jpeg", price: 599.99, location: "Amazon"),
+  Item(imgPath: "assets/img/8.jpeg", price: 699.99, location: "Amazon"),
 ];
