be30dd678e391621be98872eea7f9c53e7b21158
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 20:55:57 +0200

    put the row with the provider in appbar file

--- lib/shared/appbar.dart
+++ lib/shared/appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:test_app/provider/cart.dart';
 
 
 class pap extends StatelessWidget {
@@ -7,6 +8,39 @@ class pap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Consumer<Cart>(builder: (context, cart, child) {
+              return Row(
+                children: [
+                  Stack(
+                    children: [
+                      Positioned(
+                        bottom: 24,
+                        child: Container(
+                            child: Text(
+                              "${cart.selectedProducts.length}",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Color.fromARGB(255, 0, 0, 0)),
+                            ),
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(211, 164, 255, 193),
+                                shape: BoxShape.circle)),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.add_shopping_cart)),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Text(
+                      "\$ ${cart.price}",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              );
+            });
   }
 }
