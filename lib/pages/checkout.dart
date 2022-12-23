b6253fe9243afffbe305f114401a919d7896f6f8
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 21:20:47 +0200

    create the statelessWidget

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -1,5 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+class Checkout extends StatelessWidget {
+  const Checkout({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
