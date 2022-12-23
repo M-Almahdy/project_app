33ea1afe419cac4edd364aa1d212f9c8bff84709
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 18:10:21 +0200

    create a widget to count consumer pays

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -117,27 +117,9 @@ class Home extends StatelessWidget {
           appBar: AppBar(actions: [
             Row(
               children: [
-                Stack(
-                  children: [
-                    Positioned(
-                      bottom: 24,
-                      child: Container(
-                        child: Text(
-                          "0",
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Color.fromARGB(255, 0, 0, 0)),
-                        ),
-                        padding: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(211, 164, 255, 193),
-                            shape: BoxShape.circle),
-                      ),
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                  ],
-                ),
+                Consumer<Cart>(builder: (context, cart, child) {
+                  return 
+                },),
                 Padding(
                   padding: const EdgeInsets.only(right: 12),
                   child: Text(
