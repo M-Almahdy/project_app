2f288a07fa11a902d8508181849715404323e627
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 10:20:31 +0200

    convert stateless to stateful to use set state

--- lib/pages/details_screen.dart
+++ lib/pages/details_screen.dart
@@ -7,9 +7,14 @@ import 'package:test_app/shared/color.dart';
 
 bool isShowMore = true;
 
-class Details extends StatelessWidget {
+class Details extends StatefulWidget {
   const Details({super.key});
 
+  @override
+  State<Details> createState() => _DetailsState();
+}
+
+class _DetailsState extends State<Details> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
