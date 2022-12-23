1d0a8fdb3c6f9f0d7274a679a9fc2fc59675967d
M-Alsaqr <m.wael1036@nub.edu.eg>
2022-12-23 17:53:36 +0200

    using function add to the new created list

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/model/items.dart';
 
 class Cart with ChangeNotifier {
   List selectedProducts = [];
+
+  add(Item product) {
+    selectedProducts.add(product);
+  }
 }
